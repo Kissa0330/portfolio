279507e764d09de1713e77a44eb59494b9f64ce8
Kissa0330 <raika4789@gmail.com>
2020-04-15 14:35:22 +0000

    js修正

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 function toggleNav() {
-  var body = document.body;
+  var menu = document.getElementById("js-menu");
   var hamburger = document.getElementById("js-hamburger");
   var blackBg = document.getElementById("js-black-bg");
 
-  hamburger.addEventListener('click', function() {
-    body.classList.toggle('nav-open');
+  hamburger.addEventListener('click', function () {
+    menu.classList.toggle('nav-open');
   });
-  blackBg.addEventListener('click', function() {
-    body.classList.remove('nav-open');
+  blackBg.addEventListener('click', function () {
+    menu.classList.remove('nav-open');
   });
 }
 toggleNav();
 
 
-function mouseOver(){
-  var content = document.getElementById("js-content");
-  var body = document.body;
+function mouseOver() {
+  var contentlist = document.getElementById("js-contents");
 
-  content.addEventListener('click', function(){
-    body.classList.toggle('mouse-over');
+  contentlist.addEventListener('mouseover', function (e) {
+    e.target.classList.add('mouse-over');
   });
-}
-mouseOver()
+  contentlist.addEventListener('mouseout', function (del) {
+    del.target.classList.remove('mouse-over');
+  });
+};
+mouseOver();
